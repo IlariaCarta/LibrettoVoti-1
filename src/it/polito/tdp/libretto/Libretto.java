0df122f4ffce56185fe2e14908d27c91f42c2563
carta <carta@SURFACE-ILA.homenet.telecomitalia.it>
2019-03-19 12:03:56 +0100

    Finito punto 4

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -44,13 +44,12 @@ public class Libretto {
 	 * @return il {@link Voto} corrispondente, oppure {@code null} se non esistente
 	 */
 	public Voto cercaEsame(String nomeEsame) {
-		for (Voto v : this.voti) {
-			if (v.getCorso().equals(nomeEsame)) {
-				return v;
-			}
-		}
-
-		return null;
+		Voto voto = new Voto(0, nomeEsame, null);
+		int pos = this.voti.indexOf(voto);
+		if(pos == -1)
+			return null;
+		else
+			return this.voti.get(pos);
 	}
 
 	/**
@@ -63,14 +62,12 @@ public class Libretto {
 	 *         voto diverso
 	 */
 	public boolean esisteGiaVoto(Voto v) {
-		Voto trovato = this.cercaEsame(v.getCorso()) ;
-		if(trovato==null)
-			return false ;
-		if(trovato.getPunti()==v.getPunti()) {
-			return true ;
-		} else {
-			return false ;
-		}
+		int pos = this.voti.indexOf(v);
+		// se non c'è ritorna -1
+		if(pos==-1)
+			return false;
+		else 
+			return (v.getPunti()== this.voti.get(pos).getPunti());
 	}
 
 }
